13e4755c2fda24354c0795190e084535393690be
hugh5 <hughdrummond11@gmail.com>
2022-10-19 23:08:10 +1000

    Q1

--- src/Q1/Solution.java
+++ src/Q1/Solution.java
@@ -1,22 +1,24 @@
 package Q1;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution {
 
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+            int pair = (target - nums[i]);
+            if(map.containsKey(pair)){
+                return new int[]{map.get(pair), i};
             }
+
+            map.put(nums[i], i);
         }
         return null;
     }
 
     public static void main(String[] args) {
         int[] ans = twoSum(new int[]{3,2,4}, 6);
-        System.out.println(Arrays.toString(ans));
+        System.out.printf("[%d, %d]", ans[0], ans[1]);
     }
 }
