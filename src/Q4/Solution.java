2aaa4838757dee180c9243ab92200778dba854b3
hugh5 <hughdrummond11@gmail.com>
2022-10-20 16:07:14 +1000

    Q4

--- src/Q4/Solution.java
+++ src/Q4/Solution.java
@@ -4,27 +4,26 @@ import java.util.*;
 
 public class Solution {
     public static double findMedianSortedArrays (int[] nums1, int[] nums2) {
-        int length = nums1.length + nums2.length - 1;
-        float mid = length / 2f;
+        float mid = (nums1.length + nums2.length - 1) / 2f;
         int[] median = new int[2];
-        int[] merge = new int[(int) Math.ceil(mid) + 1];
+        int current;
 
         int a = 0, b = 0;
         for (int i = 0; i <= Math.ceil(mid); i++) {
             if (a >= nums1.length) {
-                merge[i] = nums2[b++];
+                current = nums2[b++];
             } else if (b >= nums2.length) {
-                merge[i] = nums1[a++];
+                current = nums1[a++];
             } else if (nums1[a] < nums2[b]) {
-                merge[i] = nums1[a++];
+                current = nums1[a++];
             } else {
-                merge[i] = nums2[b++];
+                current = nums2[b++];
             }
             if (i == Math.floor(mid)) {
-                median[0] = merge[i];
+                median[0] = current;
             }
             if (i == Math.ceil(mid)) {
-                median[1] = merge[i];
+                median[1] = current;
             }
         }
         return (median[0] + median[1]) / 2.0;
