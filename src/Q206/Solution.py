624ef29f1778ab65523b12c89e26accb56bf6f41
hugh5 <hughdrummond11@gmail.com>
2023-04-07 01:46:02 +1000

    Q206

--- src/Q206/Solution.py
+++ src/Q206/Solution.py
@@ -1,7 +1,16 @@
 from typing import Optional
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-class Solution
+class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
             return None
