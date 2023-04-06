d04dfc9beae5110875b5b9f591e35f1ef90745a0
hugh5 <hughdrummond11@gmail.com>
2023-04-07 01:49:34 +1000

    Q141

---
+++ src/Q141/Solution.py
@@ -0,0 +1,30 @@
+"""
+
+"""
+from typing import Optional
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following 
+the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. 
+Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        a = head
+        b = head
+        while b is not None and b.next is not None:
+            a = a.next
+            b = b.next.next
+            if a == b:
+                return True
+        return False
